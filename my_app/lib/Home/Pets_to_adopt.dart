a1666d70d2ff593ddf569a42a4e788097a66526f
Rayal kc <kcrayalgw@gmail.com>
2023-05-30 22:07:42 +0545

    Added new assets and images to the app

--- my_app/lib/Home/Pets_to_adopt.dart
+++ my_app/lib/Home/Pets_to_adopt.dart
@@ -11,6 +11,7 @@ class _PetAdoptChoicesState extends State<PetAdoptChoices> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.red,
       height: 320,
       child: PageView.builder(
           itemCount: 5,
@@ -21,15 +22,29 @@ class _PetAdoptChoicesState extends State<PetAdoptChoices> {
   }
 
   Widget _buildPageItem(int index) {
-    return GestureDetector(
-      child: Container(
+    return Stack(children: [
+      Container(
         height: 220,
         margin: EdgeInsets.only(left: 5, right: 5),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(30),
           color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+          image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage("assets/Images/shelther1.jpg")),
         ),
       ),
-    );
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          height: 130,
+          margin: EdgeInsets.only(left: 30, right: 30, bottom: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+          ),
+        ),
+      )
+    ]);
   }
 }
