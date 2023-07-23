f85027a49cf903696552e6ac4aaa7db7d4227f57
Rayal Khatri <kcrayalgw@gmail.com>
2023-07-23 08:35:28 +0545

    Final Loading Popup

--- symptom_analysis/lib/Widget/loading_popup.dart
+++ symptom_analysis/lib/Widget/loading_popup.dart
@@ -3,18 +3,24 @@ import 'package:flutter/material.dart';
 class LoadingPopup extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      content: Container(
-          child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Image.asset('assets/cat.gif',
-              height: 100,
-              width: 100), // Replace 'loading.gif' with your GIF file name
-          SizedBox(height: 16),
-          Text('Loading...'),
-        ],
-      )),
+    return Dialog(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      child: Center(
+        child: Container(
+          padding: EdgeInsets.all(0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Image.asset(
+            'assets/cat.gif',
+            height: 200,
+            width: 200,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
     );
   }
 }
